b9ad0d93aeeb730708deba31b67ac33fe7e8c57e
AFen-1 <2608216221@qq.com>
2025-07-11 13:54:21 +0800

    Update reserve.py

--- utils/reserve.py
+++ utils/reserve.py
@@ -220,6 +220,10 @@ class reserve:
             "roomId": roomid,
             "startTime": times[0],
             "endTime": times[1],
+            "startTime": times[2],
+            "endTime": times[3],
+            "startTime": times[4],
+            "endTime": times[5],
             "day": str(day),
             "seatNum": seatid,
             "captcha": captcha,
@@ -234,48 +238,3 @@ class reserve:
         logging.info(json.loads(html))
         return json.loads(html)["success"]
         
-        def get_submit(self, url, times, token, roomid, seatid, captcha="", action=False):
-        delta_day = 1 if self.reserve_next_day else 0
-        day = datetime.date.today() + datetime.timedelta(days=0+delta_day)  # 预约今天，修改days=1表示预约明天
-        if action:
-            day = datetime.date.today() + datetime.timedelta(days=1+delta_day)  # 由于action时区问题导致其早+8区一天
-        parm = {
-            "roomId": roomid,
-            "startTime": times[2],
-            "endTime": times[3],
-            "day": str(day),
-            "seatNum": seatid,
-            "captcha": captcha,
-            "token": token
-        }
-        logging.info(f"submit parameter {parm} ")
-        parm["enc"] = enc(parm)
-        html = self.requests.post(
-            url=url, params=parm, verify=True).content.decode('utf-8')
-        self.submit_msg.append(
-            times[2] + "~" + times[3] + ':  ' + str(json.loads(html)))
-        logging.info(json.loads(html))
-        return json.loads(html)["success"]
-        
-        def get_submit(self, url, times, token, roomid, seatid, captcha="", action=False):
-        delta_day = 1 if self.reserve_next_day else 0
-        day = datetime.date.today() + datetime.timedelta(days=0+delta_day)  # 预约今天，修改days=1表示预约明天
-        if action:
-            day = datetime.date.today() + datetime.timedelta(days=1+delta_day)  # 由于action时区问题导致其早+8区一天
-        parm = {
-            "roomId": roomid,
-            "startTime": times[4],
-            "endTime": times[5],
-            "day": str(day),
-            "seatNum": seatid,
-            "captcha": captcha,
-            "token": token
-        }
-        logging.info(f"submit parameter {parm} ")
-        parm["enc"] = enc(parm)
-        html = self.requests.post(
-            url=url, params=parm, verify=True).content.decode('utf-8')
-        self.submit_msg.append(
-            times[4] + "~" + times[5] + ':  ' + str(json.loads(html)))
-        logging.info(json.loads(html))
-        return json.loads(html)["success"]
