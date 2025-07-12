34172b7797d0de7ca8a985641231fcdd9e4b004e
AFen-1 <2608216221@qq.com>
2025-07-13 00:08:18 +0800

    Update encrypt.py

--- utils/encrypt.py
+++ utils/encrypt.py
@@ -7,19 +7,25 @@ import random
 from uuid import uuid1
 
 def AES_Encrypt(data):
+    # 正确的密钥和初始化向量
     key = b"u2oh6Vu^HWe4_AES"
-    # 修正 IV 值为正确的初始化向量
-    iv = b'\x01\x02\x03\x04\x05\x06\x07\x08\t\x00\x01\x02\x03\x04\x05\x06'
+    iv = b'\x01\x02\x03\x04\x05\x06\x07\x08\t\x00\x01\x02\x03\x04\x05\x06'  # 正确的IV
     
+    # 创建填充器
     padder = padding.PKCS7(128).padder()
+    
+    # 填充数据
     padded_data = padder.update(data.encode('utf-8')) + padder.finalize()
     
+    # 创建加密器
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     encryptor = cipher.encryptor()
+    
+    # 加密数据
     encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
     
+    # Base64编码
     return base64.b64encode(encrypted_data).decode('utf-8')
-    return enctext
     
 def resort(submit_info):
     return {key: submit_info[key] for key in sorted(submit_info.keys())}
