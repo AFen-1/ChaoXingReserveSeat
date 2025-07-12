cb2b6a2faf5c14c3c9acc8e4ce5a8e26f541e80e
AFen-1 <2608216221@qq.com>
2025-07-12 21:29:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -128,9 +128,9 @@ def main(users, action=False):
         logging.info("账号登录完成")
         
         # 第二步：严格等待到北京时间21:05:00
-        logging.info("严格等待到北京时间21:29:00...")
-        wait_until("21:29:00", action)
-        logging.info("北京时间21:29:00 - 开始预约流程")
+        logging.info("严格等待到北京时间21:30:00...")
+        wait_until("21:30:00", action)
+        logging.info("北京时间21:30:00 - 开始预约流程")
     
     # 非GitHub Actions模式
     else:
