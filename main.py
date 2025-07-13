55f63073e74742f1cbcef976922ac73475860ee0
AFen-1 <2608216221@qq.com>
2025-07-13 08:12:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def wait_until(target_time, action):
         time.sleep(0.1)
 
 SLEEPTIME = 0.2
-ENDTIME = "00:59:00"
+ENDTIME = "22:59:00"
 ENABLE_SLIDER = True
 MAX_ATTEMPT = 1
 RESERVE_TOMORROW = True  # 使用正确的变量名
@@ -127,9 +127,9 @@ def main(users, action=False):
         logging.info("检测到GitHub Actions模式，执行精确时间控制")
         
         # 第一步：严格等待到北京时间23:10:00
-        logging.info("严格等待到北京时间00:10:00...")
-        wait_until("00:10:00", action)
-        logging.info("北京时间00:10:00 - 开始登录账号")
+        logging.info("严格等待到北京时间08:14:00...")
+        wait_until("08:14:00", action)
+        logging.info("北京时间08:14:00 - 开始登录账号")
         
         # 获取环境变量中的账号密码
         usernames, passwords = get_user_credentials(action)
@@ -140,9 +140,9 @@ def main(users, action=False):
         logging.info("账号登录完成")
         
         # 第二步：严格等待到北京时间21:30:00
-        logging.info("严格等待到北京时间00:11:00...")
-        wait_until("00:11:00", action)
-        logging.info("北京时间00:11:00 - 开始预约流程")
+        logging.info("严格等待到北京时间08:15:00...")
+        wait_until("08:15:00", action)
+        logging.info("北京时间08:15:00 - 开始预约流程")
     
     # 非GitHub Actions模式
     else:
