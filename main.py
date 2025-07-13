0c60c8f6902e908d7423dfdc2cfdeaef55114ec4
AFen-1 <2608216221@qq.com>
2025-07-13 23:16:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -127,9 +127,9 @@ def main(users, action=False):
         logging.info("检测到GitHub Actions模式，执行精确时间控制")
         
         # 第一步：严格等待到北京时间23:10:00
-        logging.info("严格等待到北京时间08:21:00...")
-        wait_until("08:21:00", action)
-        logging.info("北京时间08:21:00 - 开始登录账号")
+        logging.info("严格等待到北京时间23:17:00...")
+        wait_until("23:17:00", action)
+        logging.info("北京时间23:17:00 - 开始登录账号")
         
         # 获取环境变量中的账号密码
         usernames, passwords = get_user_credentials(action)
@@ -140,9 +140,9 @@ def main(users, action=False):
         logging.info("账号登录完成")
         
         # 第二步：严格等待到北京时间21:30:00
-        logging.info("严格等待到北京时间08:22:00...")
-        wait_until("08:22:00", action)
-        logging.info("北京时间08:22:00 - 开始预约流程")
+        logging.info("严格等待到北京时间23:17:30...")
+        wait_until("23:17:30", action)
+        logging.info("北京时间23:17:30 - 开始预约流程")
     
     # 非GitHub Actions模式
     else:
